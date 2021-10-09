e09ba998ea809894ce5ee76a90fcbe11aa1ddde5
jrcadi <hrkhttr@gmail.com>
2021-10-09 23:05:32 +0900

    d*.sh change

--- dnormal.sh
+++ dnormal.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 xrandr --output eDP-1 --rotate normal
-xinput set-prop '14' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
+xinput set-prop '19' 'Coordinate Transformation Matrix' 1 0 0 0 1 0 0 0 1
