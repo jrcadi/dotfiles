d9adc9e58423711eb3feef03bd0544ed61e1503c
jrcadi <hrkhttr@gmail.com>
2021-11-25 20:35:23 +0900

    inputrc

--- setting.sh
+++ setting.sh
@@ -6,7 +6,7 @@ cp .inputrc ~
 cp .gitconfig ~
 cd
 sudo apt update
-sudo apt install vim xinput hardinfo git
+sudo apt install vim xinput hardinfo git guake
 
 #デュアルブート時の時刻ズレ修正
 sudo hwclock -D --systohc --localtime
