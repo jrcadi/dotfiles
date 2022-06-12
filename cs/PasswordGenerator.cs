78f927a7d4abd19489e8aaba78daa2997515e218
jrcadi <hrkhttr+i@gmail.com>
2022-06-12 20:38:28 +0900

    2-1

---
+++ cs/PasswordGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// このクラスはProgramクラスのMainメソッドで使用される
+/// </summary>
+namespace PasswordCreator {
+    public class PasswordGenerator {
+        public string MakePassword(Random random, int count) {
+            //剰余を利用して、数字、英小文字、英大文字、記号の順に引数countの値の文字数をランダムに生成する
+            string password = string.Empty;
+            for(int i = 0; i < count; i++) {
+                int n = i % 4;
+                if(n == 0) password += GetNumLetter(random);
+                if(n == 1) password += GetLowerLetter(random);
+                if(n == 2) password += GetUpperLetter(random);
+                if(n == 3) password += GetMarkLetter(random);
+            }
+            return password;
+        }
+        private string GetNumLetter(Random random) {
+            return random.Next(10).ToString();
+        }
+        private string GetLowerLetter(Random random) {
+            char c = (char)random.Next(97, 123);
+            return c.ToString();
+        }
+        private string GetUpperLetter(Random random) {
+            char c = (char)random.Next(65, 91);
+            return c.ToString();
+        }
+        private string GetMarkLetter(Random random) {
+            string[] mark = { "!", "#", "$", "%", "&", "@" };
+            int n = random.Next(mark.Length);
+            return mark[n];
+        }
+    }
+}
--- cs/Program.cs
+++ cs/Program.cs
@@ -1,54 +1,19 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /// <summary>
-
+/// Chapter2-1 クラスを使おう！（前半　P.47まで）
 /// </summary>
-namespace test02 {
+namespace PasswordCreator {
     class Program {
         static void Main(string[] args) {
             Random random = new Random();
             int count = 12;
-            string password = MakePassword(random, count);
+            PasswordGenerator generator = new PasswordGenerator();//インスタンスを生成
+            string password = generator.MakePassword(random, count);
             Console.WriteLine(password);
-        }
-        private static string MakePassword(Random random, int count) {
-            string password = string.Empty;
-            for(int i=0; i<count; i++){
-                int n = i % 4;
-                if(n == 0){
-                    password += GetNumLetter(random);
-                }
-                if(n == 1){
-                    password += GetLowerLetter(random);
-                }
-                if(n == 2){
-                    password += GetUpperLetter(random);
-                }
-                if(n == 3){
-                    password += GetMarkLetter(random);
-                }
-            }
-            return password;
-        }
-        //以下の4つのメソッドはMakePasswordメソッドで呼び出される
-        private static string GetNumLetter(Random random) {
-            return random.Next(10).ToString();
-        }
-        private static string GetLowerLetter(Random random) {
-            char c = (char)random.Next(97, 123);
-            return c.ToString();
-        }
-        private static string GetUpperLetter(Random random) {
-            char c = (char)random.Next(65, 91);
-            return c.ToString();
-        }
-        private static string GetMarkLetter(Random random) {
-            string[] mark = { "!", "#", "$", "%", "&", "@" };
-            int n = random.Next(mark.Length);
-            return mark[n];
         }
     }
 }
